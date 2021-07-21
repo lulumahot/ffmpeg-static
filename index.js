fa97048a9967d65ac1559fab46e1e112f1b0fbfe
lulumahot <lulumahot@hotmail.fr>
2021-07-21 11:56:53 +0200

    Change to ffprobe

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-if (process.env.FFMPEG_BIN) {
-  module.exports = process.env.FFMPEG_BIN
+if (process.env.FFPROBE_BIN) {
+  module.exports = process.env.FFPROBE_BIN
 } else {
   var os = require('os')
   var path = require('path')
@@ -16,14 +16,14 @@ if (process.env.FFMPEG_BIN) {
   var platform = process.env.npm_config_platform || os.platform()
   var arch = process.env.npm_config_arch || os.arch()
 
-  var ffmpegPath = path.join(
+  var ffprobePath = path.join(
     __dirname,
-    platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg'
+    platform === 'win32' ? 'ffprobe.exe' : 'ffprobe'
   )
 
   if (!binaries[platform] || binaries[platform].indexOf(arch) === -1) {
-    ffmpegPath = null
+    ffprobePath = null
   }
 
-  module.exports = ffmpegPath
+  module.exports = ffprobePath
 }
